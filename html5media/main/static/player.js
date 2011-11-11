2f17f2b9087589f84b6ee4d1be8ad7ef1dce243b
David Powell <pwnzerdragoon@gmail.com>
2011-11-11 14:06:35 -0800

    Player autoadvances queue

--- html5media/main/static/player.js
+++ html5media/main/static/player.js
@@ -11,33 +11,44 @@
  * code uses to control the player. There should only be one instance
  * of the queue, because more than one doesn't make sense. If for some
  * reason you need more you should probably just use a regular playlist */
-function Queue() { }
-Queue.prototype.playItem = function( trackIndex ) {
-    var url = this.playlist[trackIndex];
-    var playerPane = document.getElementById("player");
-    playerPane.innerHTML = "<audio src=" + url + " type='audio/ogg' controls autoplay />";
-    var playerMeta = document.getElementById("metadata");
-    playerMeta.innerHTML = url;
+function Queue() {
+    this.currentlyPlaying = null;
 }
-Queue.prototype.setPlaylist = function( playlist ) {
-    this.playlist = playlist;
-}
-Queue.prototype.updatePage = function() {
-    var queuePane = document.getElementById("queue");
-    var queueList = document.createElement("ul");
-    for (tracknum in this.playlist) {
-        track = this.playlist[tracknum];
-        var trackItem = document.createElement("li");
-        trackItem.trackIndex = tracknum;
-        trackItem.onclick = function(){
-            queue.playItem(this.trackIndex);
-            this.classList.add("currentlyPlaying");
+    Queue.prototype.playItem = function( trackIndex ) {
+        var url = this.playlist[trackIndex];
+        var player = document.getElementById("audioPlayer");
+        player.src = url;
+        player.play();
+        var playerMeta = document.getElementById("metadata");
+        playerMeta.innerHTML = url;
+        // Update currently playing highlight
+        if (this.currentlyPlaying != null) {
+            this.listElements[this.currentlyPlaying].classList.remove("currentlyPlaying");
         }
-        trackItem.appendChild(document.createTextNode(track));
-        queueList.appendChild(trackItem);
+        this.listElements[trackIndex].classList.add("currentlyPlaying");
+        this.currentlyPlaying = parseInt(trackIndex);
     }
-    queuePane.appendChild(queueList);
-}
+    Queue.prototype.setPlaylist = function( playlist ) {
+        this.playlist = playlist;
+    }
+    Queue.prototype.updatePage = function() {
+        // listElements is used to associate playlist items with DOM elements
+        this.listElements = [];
+        var queuePane = document.getElementById("queue");
+        var queueList = document.createElement("ul");
+        for (tracknum in this.playlist) {
+            track = this.playlist[tracknum];
+            var trackItem = document.createElement("li");
+            trackItem.trackIndex = tracknum;
+            trackItem.onclick = function(){
+                queue.playItem(this.trackIndex);
+            }
+            this.listElements.push(trackItem);
+            trackItem.appendChild(document.createTextNode(unescape(track.replace("/static/", ""))));
+            queueList.appendChild(trackItem);
+        }
+        queuePane.appendChild(queueList);
+    }
 
 
 /* Functions
@@ -50,12 +61,23 @@ function requestPlaylist() {
     return JSON.parse(request.responseText);
 }
 
+// Called when a track is done playing and starts the next track
+function trackFinished() {
+    if (queue.currentlyPlaying < queue.playlist.length) {
+        //TODO: vary behavior depending on options (autoplay off, shuffle, etc.)
+        queue.playItem(queue.currentlyPlaying + 1);
+    }
+}
+
 
 /* Init code 
  ************/
 window.onload = function() {
     // Instance the queue
     window.queue = new Queue();
+    
+    // Sent up events
+    document.getElementById("audioPlayer").onended = trackFinished;
     
     // Get default playlist
     var playlist = requestPlaylist();
