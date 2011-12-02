db086a2f92d473de815aaf0cc2443490fd1cddab
David Powell <pwnzerdragoon@gmail.com>
2011-12-01 21:47:45 -0800

    Added Artist and Album to admin section

--- html5media/backends/database/admin.py
+++ html5media/backends/database/admin.py
@@ -1,5 +1,7 @@
-from models import Media, Playlist
+from models import *
 from django.contrib import admin
 
 admin.site.register(Media)
 admin.site.register(Playlist)
+admin.site.register(Album)
+admin.site.register(Artist)
