2f8992393833e7dea934fbf619ca9799d7d08bc0
David Powell <pwnzerdragoon@gmail.com>
2011-12-06 20:27:26 -0800

    Logging on the home page

--- html5media/player/views.py
+++ html5media/player/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render_to_response
+import logging
+
+# Set up logging
+logger = logging.getLogger("apps")
 
 def index(request):
+    logger.info("Home page requested from {}".format(request.get_host()))
     return render_to_response("main.html.djt", {})
