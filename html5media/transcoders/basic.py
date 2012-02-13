42a9a37af99d7588cf5408e2686fc9ec0756a5f2
David Powell <pwnzerdragoon@gmail.com>
2012-02-12 22:38:07 -0800

    Made transcoder manager more generic
    As much common code as possible was moved into the base class. General code cleanup was done as well, along with some improved documentation with an eye to supporting Sphinx's autodoc in the future.

--- html5media/transcoders/basic.py
+++ html5media/transcoders/basic.py
@@ -2,11 +2,8 @@
 # Simple transcoder module that ensures each audio file has an ogg and mp3 version
 
 import os
-from django.conf import settings
 
 from common import TranscodeManagerBase
-# Encoding engine is defined in the settings, import it dynamically
-encode = __import__("transcoders.encoders." + settings.ENCODER, fromlist=["encode"]).encode
 
 
 class TranscodeManager(TranscodeManagerBase):
@@ -14,42 +11,19 @@ class TranscodeManager(TranscodeManagerBase):
     
     def __init__(self, filename):
         TranscodeManagerBase.__init__(self, filename)
-        self._transcodes = []
+        
         if self.filename.endswith(".ogg"):
-            self._transcodename = self.make_transcode_name(self.filename, ".mp3")
+            transcodename = self.make_transcode_name(self.filename, ".mp3")
+            transcodemime = "audio/mp3"
         elif self.filename.endswith(".mp3"):
-            self._transcodename = self.make_transcode_name(self.filename, ".ogg")
+            transcodename = self.make_transcode_name(self.filename, ".ogg")
+            transcodemime = "audio/ogg"
         else:
             #TODO: raise an error here
             pass
         # If the transcoded file already exists, add it to the list of transcodes
-        if os.path.exists(self._transcodename):
-            self._transcodes.append( (self._transcodename,
-                                      "audio/" + self._transcodename[-3:]) )
-    
-    def convert(self):
-        """Executes a transcoding job, if needed"""
-        
-        # This basic transcoder creates MP3s out of OGGs, and vice versa.
-        if self.filename.endswith(".ogg"):
-            newmime = "audio/mp3"
-        elif self.filename.endswith(".mp3"):
-            newmime = "audio/ogg"
-        
-        encode(self.filename, self._transcodename, newmime)
-        self._transcodes.append( (self._transcodename, newmime) )
-    
-    @property
-    def transcode_needed(self):
-        return not os.path.exists(self._transcodename)
-    
-    @property
-    def files(self):
-        """Returns information about all files associated with the transcoder"""
-        #TODO: _transcodes could probably be tweaked to make this function simpler
-        relname = self.filename.replace(settings.AUDIO_ROOT, "")
-        output = [(relname, self._fileurl(self.filename), "audio/" + relname[-3:])]
-        for transcode in self._transcodes:
-            relname = transcode[0].replace(settings.TRANSCODE_ROOT, "")
-            output.append( (relname, self._transcodeurl(transcode[0]), transcode[1]) )
-        return output
+        if os.path.exists(transcodename):
+            self.transcodes.append( (transcodename, transcodemime ) )
+        else:
+            # Otherwise add it to the job list
+            self.pendingJobs.append(transcodename)
--- html5media/transcoders/common.py
+++ html5media/transcoders/common.py
@@ -2,25 +2,81 @@
 
 import os
 from django.conf import settings
+# Encoding engine is defined in the settings, import it dynamically
+encode = __import__("transcoders.encoders." + settings.ENCODER, fromlist=["encode"]).encode
 
 
 class TranscodeManagerBase(object):
-    """Base class for all transcoder managers"""
+    """
+    Base class for all transcoder managers
+    """
     
     def __init__(self, filename):
+        """Basic manager setup. As-is will perform a null transcoding job,
+           make sure your subclasses define an __init__ to set things up."""
+        
+        #: Input filename.
         self.filename = filename
+        #: List of transcoder output filenames to be processed.
+        self.pendingJobs = []
+        #: List of all found transcoded media files.
+        #: Formatted as (path, mimetype) pairs in a list.
+        self.transcodes = []
+        #TODO: Add sources[], to allow for multiple inputs
+    
+    def convert(self):
+        """Executes a transcoding job, if needed"""
+        for job in self.pendingJobs:
+            #TODO: A more extensive and flexible MIME system
+            if job.endswith(".ogg"):
+                newmime = "audio/ogg"
+            elif job.endswith(".mp3"):
+                newmime = "audio/mp3"
+        
+            encode(self.filename, job, newmime)
+            self.transcodes.append( (job, newmime) )
     
     def make_transcode_name(self, path, newext):
-        # Output name is the basename prefixed with parent directory names,
-        # separated by periods. This keeps all transcodes in the same
-        # place, and also prevents name collisions.
+        """
+        Generates a transcoded filename from a given path
+        
+        Output name is the basename prefixed with parent directory names,
+        separated by periods. This keeps all transcodes in the same
+        place, and also prevents name collisions. In addition to all this
+        name mangling, it also replaces AUDIO_ROOT with TRANSCODE_ROOT.
+        """
         
         # Note the slice, it strips away the initial path seperator
         outname = path.replace(settings.AUDIO_ROOT, "")[1:].replace(os.sep, ".")
         outname = os.path.join(settings.TRANSCODE_ROOT,
                                os.path.splitext(outname)[0] + ".transcode" + newext)
-        
         return outname
+    
+    @property
+    def transcode_needed(self):
+        """Returns ``True`` if transcodes need to be performed."""
+        return True if self.pendingJobs else False
+    
+    @property
+    def files(self):
+        """
+        Returns information about all files associated with the transcoder.
+        
+        Data is in the form of a list of tuples, each tuple a triplet of three
+        values containing the media's pathname (relative to its root defined
+        in the settings), the URL used to access the media, and the MIME type.
+        """
+        
+        # Start with the source file(s)
+        relname = self.filename.replace(settings.AUDIO_ROOT, "")
+        output = [(relname, self._fileurl(self.filename), "audio/" + relname[-3:])]
+        
+        # Then add the transcode(s)
+        for transcode in self.transcodes:
+            relname = transcode[0].replace(settings.TRANSCODE_ROOT, "")
+            output.append( (relname, self._transcodeurl(transcode[0]), transcode[1]) )
+        
+        return output
     
     def _fileurl(self, path):
         """Returns the URL used to access a given path (for original files)"""
