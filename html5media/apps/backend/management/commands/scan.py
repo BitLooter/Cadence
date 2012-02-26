5cff0d3a9fea5d2b87c20a46043a30736fd773cf
David Powell <pwnzerdragoon@gmail.com>
2012-02-26 00:06:31 -0800

    Revamped scanner system

--- html5media/apps/backend/management/commands/scan.py
+++ html5media/apps/backend/management/commands/scan.py
@@ -1,7 +1,19 @@
-from django.core.management.base import BaseCommand, CommandError
-from html5media.apps.backend.scanner import scan
+from optparse import make_option
+
+from django.core.management.base import NoArgsCommand, CommandError
+from html5media.apps.backend.scanner import Scanner
 
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        scan()
+class Command(NoArgsCommand):
+    """Django management command for scanning media files"""
+    
+    help = "Scans the directories specified in your Django settings for media files."
+    option_list = NoArgsCommand.option_list + (
+        make_option('-f', '--force-transcode',
+            action='store_true', dest='force_transcode', default=False,
+            help='The scanner will transcode media even if transcodes are already present, overwriting existing files.'),
+    )
+    
+    def handle_noargs(self, **options):
+        scanner = Scanner(options)
+        scanner.scan()
--- html5media/apps/backend/scanner.py
+++ html5media/apps/backend/scanner.py
@@ -13,7 +13,6 @@ TranscodeManager = __import__(settings.TRANSCODER, fromlist=["TranscodeManager"]
 logger = logging.getLogger('apps')
 
 
-#TODO: remove (object) in Py3k
 class Mediainfo(object):
     def __init__(self, pathname):
         # Use easy mode for ID3 tagged files, we don't need direct ID3 access
@@ -34,99 +33,133 @@ class Mediainfo(object):
            or False if not present in database"""
         try:
             return os.path.getmtime(self.path) > \
-                   time.mktime(models.MediaSource.objects.get(path=self.relpath).media.scanDate.timetuple())
+                   time.mktime(models.MediaSource.objects.get(path=self.relpath).media.scan_date.timetuple())
         except models.Media.DoesNotExist:
             return False
 
-def scan():
-    """
-    Scans a directory for media and updates the database.
+
+class Scanner(object):
+    def __init__(self, options):
+        self.options = options
     
-    Note that because it uses Django models to interface with the database,
-    it must be run inside a Django context, using "manage.py scan".
-    """
+    def scan(self):
+        """
+        Scans a directory for media and updates the database.
+        
+        Note that because it uses Django models to interface with the database,
+        it must be run inside a Django context, using "manage.py scan".
+        """
+        
+        logger.info("Media scan initiated")
+        
+        # Get metadata for every file in the media directory (recursively)
+        pathnames = []
+        for dirpath, dirnames, filenames in os.walk(settings.AUDIO_ROOT):
+            pathnames += [os.path.join(dirpath, f) for f in filenames if os.path.splitext(f)[1] in TranscodeManager.source_types]
+        meta = [Mediainfo(p) for p in pathnames]
+        
+        # Process available album art
+        self.get_art()
+        
+        # Create the album and artist database entries
+        # Make a list of every item. Use a set to filter out duplicates.
+        albums = set([a.album for a in meta])
+        artists = set([a.artist for a in meta])
+        # Correct for blank tags
+        if "" in albums:
+            albums.remove(""); albums.add(settings.UNKNOWN_ALBUM)
+        if "" in artists:
+            artists.remove(""); artists.add(settings.UNKNOWN_ARTIST)
+        # Create the entires in the database
+        self.make_albums(albums)
+        self.make_artists(artists)
+        
+        # Now process the scanned files
+        # Make a list of all previously processed files
+        #TODO: add a flag for transcodes to the database
+        self.scannedFiles = models.Media.objects.values_list("original_source", flat=True)
+        #TODO: iteritems() becomes items() in Py3k
+        for metadata in meta:
+            media = self.update_db(metadata)
+            # Do the media transcoding, if needed
+            self.make_transcodes(metadata.path, media)
+            
+        logger.info("Media scan complete")
     
-    logger.info("Media scan initiated")
+    def get_art(self):
+        artFilenames = os.listdir(settings.ALBUMART_ROOT)
+        self.artUrls = {}
+        # Map file basenames to resulting URLS
+        for image in artFilenames:
+            name = os.path.splitext(image)[0]
+            self.artUrls[name] = urllib.quote(settings.ALBUMART_URL + image)
     
-    pathnames = []
-    for dirpath, dirnames, filenames in os.walk(settings.AUDIO_ROOT):
-        pathnames += [os.path.join(dirpath, f) for f in filenames if os.path.splitext(f)[1] in TranscodeManager.source_types]
-    meta = {p: Mediainfo(p) for p in pathnames}
-    albums = set([a.album for a in meta.values()])
-    artists = set([a.artist for a in meta.values()])
-    # Correct for blank tags
-    if "" in albums:
-        albums.remove(""); albums.add(settings.UNKNOWN_ALBUM)
-    if "" in artists:
-        artists.remove(""); artists.add(settings.UNKNOWN_ARTIST)
+    def make_artists(self, artists):
+        self.artistEntries = {}
+        for artist in artists:
+            self.artistEntries[artist] = models.Artist.objects.get_or_create(name=artist)[0]
     
-    # Process available album art
-    artFilenames = os.listdir(settings.ALBUMART_ROOT)
-    artUrls = {}
-    # Tie file basenames to resulting URLS
-    for image in artFilenames:
-        name = os.path.splitext(image)[0]
-        artUrls[name] = urllib.quote(settings.ALBUMART_URL + image)
+    def make_albums(self, albums):
+        self.albumEntries = {}
+        for album in albums:
+            # Check for album art
+            # Album name may contain bad path characters (like ':'), search against a filtered version
+            pathalbum = self.filter_path_chars(album)
+            if pathalbum in self.artUrls:
+                cover = self.artUrls[pathalbum]
+            else:
+                cover = ""
+            self.albumEntries[album] = models.Album.objects.get_or_create(name=album, coverurl=cover)[0]
     
-    # Create the album and artist database entries
-    albumEntries = {}
-    for album in albums:
-        # Check for album art
-        # Album name may contain bad path characters (like ':'), search against a filtered version
-        pathalbum = filterPathChars(album)
-        if pathalbum in artUrls:
-            cover = artUrls[pathalbum]
-        else:
-            cover = ""
-        albumEntries[album] = models.Album.objects.get_or_create(name=album, coverurl=cover)[0]
-    artistEntries = {}
-    for artist in artists:
-        artistEntries[artist] = models.Artist.objects.get_or_create(name=artist)[0]
-    
-    # Now process the media files
-    # Get a list of all previously processed files
-    scannedFiles = models.MediaSource.objects.exclude(path__contains=".transcode").values_list("path", flat=True)
-    for filename in pathnames:
+    def update_db(self, metadata):
         # Database paths are relative to AUDIO_ROOT
-        metadata = meta[filename]
-        if metadata.relpath not in scannedFiles:
+        if metadata.relpath not in self.scannedFiles:
             # New file not in database, create new entry
             media = models.Media()
-            print("Found new media: " + filename.replace(settings.AUDIO_ROOT, ""))
+            print("Found new media: " + metadata.path.replace(settings.AUDIO_ROOT, ""))
+            update_needed = True
         elif metadata.modified:
             # In the database but modified since last scan
-            print("Modified media: " + filename.replace(settings.AUDIO_ROOT, ""))
-            media = models.Media.objects.get(path=metadata.relpath)
+            media = models.MediaSource.objects.get(path=metadata.relpath).media
+            print("Modified media: " + metadata.path.replace(settings.AUDIO_ROOT, ""))
+            update_needed = True
         else:
-            # If already in database and not modified, skip to the next one
-            continue
+            # Else nothing to do here, but do get the object to return later
+            media = models.MediaSource.objects.get(path=metadata.relpath).media
+            print("Skipping already known media: " + metadata.path.replace(settings.AUDIO_ROOT, ""))
+            update_needed = False
         
-        # Do the media transcoding, if needed
+        # Only update if this is new or modifed data
+        if update_needed == True:
+            # Save all the info to the database
+            media.title = metadata.title
+            media.artist = self.artistEntries[metadata.artist]
+            media.album = self.albumEntries[metadata.album]
+            media.length = metadata.length
+            media.original_source = metadata.path.replace(settings.AUDIO_ROOT, "")
+            media.save()
+        
+        return media
+    
+    def make_transcodes(self, filename, media):
         transcoder = TranscodeManager(filename)
-        if transcoder.transcode_needed:
+        
+        # By default transcode only if needed, but can be forced by command line
+        if transcoder.transcode_needed or self.options["force_transcode"]:
             print("Transcoding...")
             transcoder.convert()
         
-        # Save all the info to the database
-        media.title = metadata.title
-        media.artist = artistEntries[metadata.artist]
-        media.album = albumEntries[metadata.album]
-        media.length = metadata.length
-        media.save()
-        
         for transpath, url, mime in transcoder.files:
-            source = models.MediaSource()
-            source.media = media
-            source.path = transpath
-            source.url = url
-            source.mime = mime
-            source.save()
+            # Create DB entries, if they do not exist already
+            create_params = {"media": media,
+                             "path":  transpath,
+                             "url":   url,
+                             "mime":  mime}
+            models.MediaSource.objects.get_or_create(**create_params)
     
-    logger.info("Media scan complete")
-
-def filterPathChars(path):
-    """Takes a string and returns it with illegal path characters removed"""
-    newpath = path
-    for char in r'\/:*?"<>|':
-        newpath = newpath.replace(char, "")
-    return newpath
+    def filter_path_chars(self, path):
+        """Takes a string and returns it with illegal path characters removed"""
+        newpath = path
+        for char in r'\/:*?"<>|':
+            newpath = newpath.replace(char, "")
+        return newpath
