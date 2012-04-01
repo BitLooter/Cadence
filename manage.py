2f3c448db490cc9ffa6f6bbfde28593b460796b3
David Powell <pwnzerdragoon@gmail.com>
2012-03-31 17:54:08 -0700

    Note for the future, look for defaultdebug first

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    #TODO: If debug doesn't exist, use defaultdebug
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cadence.conf.debug")
     
     from django.core.management import execute_from_command_line
