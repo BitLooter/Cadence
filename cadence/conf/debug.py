6cc3a467392812d937f80271a5ba952a7a051a25
David Powell <pwnzerdragoon@gmail.com>
2012-04-02 22:06:16 -0700

    Enabled Django timezones

--- cadence/conf/debug.py
+++ cadence/conf/debug.py
@@ -11,6 +11,8 @@ SECRET_KEY = 'w+u!vatz60^t@+pocdm8%g!ps_)heb@aj$4g_ptiqnol^c4bdh'
 STATIC_ROOT = "C:\\Develop\\htdocs\\cadence\\static"
 MEDIA_ROOT = "C:\\Develop\\htdocs\\cadence\\media"
 
+USE_TZ=True
+
 # Reset Cadence paths to the new MEDIA_ROOT
 AUDIO_ROOT = os.path.join(MEDIA_ROOT, "music\\")
 TRANSCODE_ROOT = os.path.join(MEDIA_ROOT, "transcodes\\")
--- cadence/conf/settings_default.py
+++ cadence/conf/settings_default.py
@@ -47,6 +47,8 @@ DATABASES = {
 USE_I18N = True
 USE_L10N = True
 
+USE_TZ=True
+
 ROOT_URLCONF = 'cadence.urls'
 
 STATIC_ROOT = "/var/www/static/"
