c5648f1d10ba2dbaaf93e0240535d430ed1c0d82
David Powell <pwnzerdragoon@gmail.com>
2012-04-18 14:43:00 -0700

    Corrected a bug preventing playlists URLs from working

--- cadence/apps/backend/urls.py
+++ cadence/apps/backend/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('cadence.apps.backend.views',
-    url(r"^playlists/(?P<playlistID>\d+)/$", "playlist"),
+    url(r"^playlists/(?P<playlistID>\d+)/$", "getplaylist"),
     url(r"^playlists/$", "playlists"),
     url(r"^library/albums/(?P<albumID>\d+)/$", "library_get_album"),
     url(r"^library/albums/$", "library_albums"),
