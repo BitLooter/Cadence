4166ee59f545f3de72e7dec213be58e7bf74638f
BitLooter <pwnzerdragoon+gh@gmail.com>
2012-12-11 14:49:16 -0800

    runserver command uses defaultdebug config if debug doesn't exist

--- manage.py
+++ manage.py
@@ -3,8 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    #TODO: If debug doesn't exist, use defaultdebug
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cadence.conf.debug")
+    # If a debugging configuration has not been set up, use the default debugging settings
+    if os.path.exists(os.path.join(os.path.dirname(__file__), "cadence/conf/debug.py")):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cadence.conf.debug")
+    else:
+        print "Debug configuration not found, using defaultdebug instead"
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cadence.conf.defaultdebug")
     
     from django.core.management import execute_from_command_line
     
