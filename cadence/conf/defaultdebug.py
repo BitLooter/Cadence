3dcc1a026644c946b823f9db9191cbccca1e4d09
David Powell <pwnzerdragoon+gh@gmail.com>
2014-05-22 20:52:30 -0700

    Added debug secret key
    DO NOT USE THIS KEY IN PRODUCTION

--- cadence/conf/defaultdebug.py
+++ cadence/conf/defaultdebug.py
@@ -8,4 +8,7 @@ from settings_default import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-INTERNAL_IPS = ('127.0.0.1',)
+INTERNAL_IPS = ('10.0.2.15',)
+
+# Warning: Never ever ever use this in production
+SECRET_KEY = "IT'S A SECRET TO EVERYBODY"
