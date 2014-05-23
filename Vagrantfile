1d3125cef2486b61d427d0359c5c3962e1a65b12
David Powell <pwnzerdragoon+gh@gmail.com>
2014-05-22 21:18:07 -0700

    Finished vagrant setup
    vagrant up in the project's root and it will get things going, binding a
    dev server to localhost:8000. Using shell provisioning for now, I may
    switch to something more sophisticated in the future if more complexity
    is required.

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,7 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "vogtmann/debian-7.5-64"
 
-  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 8000, host: 8000
 
   config.vm.synced_folder "./", "/vagrant"
   
--- provision.sh
+++ provision.sh
@@ -2,3 +2,14 @@
 
 sudo apt-get -y install python-pip
 sudo pip install -r /vagrant/reqs.txt
+sudo echo "#!/bin/sh" > /etc/rc.local
+sudo echo "/home/vagrant/startserver.sh" >> /etc/rc.local
+sudo echo "exit 0" >> /etc/rc.local
+echo "#!/bin/sh" > /home/vagrant/startserver.sh
+# Wait for /vagrant to be mounted
+echo "while [ ! -f /vagrant/manage.py ]; do sleep 1; done" >> /home/vagrant/startserver.sh
+echo "cd /vagrant" >> /home/vagrant/startserver.sh
+echo "/usr/bin/python manage.py runserver 0.0.0.0:8000 &" >> /home/vagrant/startserver.sh
+chmod 755 /home/vagrant/startserver.sh
+# Start everything after initial boot+provision
+/home/vagrant/startserver.sh
